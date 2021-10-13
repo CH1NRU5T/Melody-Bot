e1c470de3c54bccb92a7392a86caa7d7ddc476e1
Ansh Agrawal <72245838+CH1NRU5T@users.noreply.github.com>
2021-10-13 06:01:15 +0000

    clean code

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const keepAlive = require('./server');
 const client = new discord.Client();
 const axios = require('axios');
 require('dotenv').config()
-
 const prefix = '=';
 const fs = require('fs');
-var songLink = ""
 
+//  Advanced file structure start
 client.commands = new discord.Collection()
 const commmandFiles = fs.readdirSync(`./commands/`).filter(file => file.endsWith(`.js`));
 
@@ -16,8 +15,7 @@ for (const file of commmandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.name, command);
 }
-
-//get song function
+// Advanced file structure end
 
 client.once(`ready`, () => {
   console.log(`MelodyBot is online`);
@@ -27,9 +25,7 @@ client.on(`message`, msg => {
   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
   const args = msg.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
-  // console.log(command);
   if (command === `play` || command === `p`) {
-    // songLink1().then(res => playSong(msg, res));
     client.commands.get(`play`).execute(msg, args);
 
   }
@@ -37,9 +33,6 @@ client.on(`message`, msg => {
     client.commands.get(`leave`).execute(msg, args);
   }
 })
-
-
-
 
 
 //login
